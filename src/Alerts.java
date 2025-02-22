51c9e9606b00377117c9a73c83f6c637873649d6
116Ashwini3065 <ashk182018@gmail.com>
2025-02-22 20:03:04 +0530

    First Commit

---
+++ src/Alerts.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+
+public class Alerts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 String a="Hello world";
+	 System.out.println(a);
+		
+		
+	}
+
+}
